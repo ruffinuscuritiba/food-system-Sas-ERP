d21542532d14632b989b539d2eab73af19a1fc1c
Ruffinus Pizzaria <ruffinuscuritiba@gmail.com>
2026-05-17 16:31:17 -0300

    corrige dependencia AuditService em produtos

--- backend/src/modules/products/products.module.ts
+++ backend/src/modules/products/products.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from 'src/database/prisma.module';
+import { AuditModule } from '@/modules/audit/audit.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuditModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })

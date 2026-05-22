593df49061fbae68e821b5aaa12966cf0aec11dd
Ruffinus Pizzaria <ruffinuscuritiba@gmail.com>
2026-05-21 22:39:16 -0300

    sync all changes

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,26 +1,18 @@
-import axios from 'axios'
+﻿import axios from 'axios'
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://food-system-backend-94zd.onrender.com/api'
 
 export const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL,
-
+  baseURL: apiUrl,
   withCredentials: true,
 })
 
-api.interceptors.request.use(
-  (config) => {
-
-    const token =
-      localStorage.getItem('token')
-
-    if (token) {
-
-      config.headers.Authorization =
-        `Bearer ${token}`
-    }
-
-    return config
-  },
-)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 export default api
